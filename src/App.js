5479cc4540e94b43aafc18ece2055b185f89ea8a
Gary McAdam <gary@mcad.am>
2018-10-25 10:49:05 +0100

    [master] untar and show imgs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import untar from "js-untar";
+import ImageGallery from "react-image-gallery";
+
+import "react-image-gallery/styles/css/image-gallery.css";
+
+import "./App.css";
 
 class App extends Component {
+  state = {
+    items: null
+  };
+
+  onUpload = e => {
+    const fileReader = new FileReader();
+    fileReader.onload = event => {
+      const arrayBuffer = event.target.result;
+      untar(arrayBuffer).then(files => {
+        this.setState({
+          items: files.map((file, i) => {
+            return {
+              original: file.getBlobUrl(),
+              originalTitle: file.name,
+              description: file.name
+            };
+          })
+        });
+      });
+    };
+    fileReader.readAsArrayBuffer(e.target.files[0]);
+  };
+
   render() {
+    const { items } = this.state;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <label htmlFor="">
+            <input type="file" onChange={this.onUpload} accept=".tar" />
+          </label>
+          {items && (
+            <ImageGallery
+              showThumbnails={false}
+              infinite={false}
+              items={items}
+              showPlayButton={false}
+            />
+          )}
         </header>
       </div>
     );
